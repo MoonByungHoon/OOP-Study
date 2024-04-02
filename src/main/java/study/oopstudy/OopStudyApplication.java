a3997035499e5c381f1ae92a38cfc82cf5a97a2c
MoonByungHoon <mbh1069@naver.com>
2024-04-03 03:41:30 +0900

    docs: Readme 파일 수정
    Readme 파일 내에 객체 관계도 그림 수정.

--- src/main/java/study/oopstudy/OopStudyApplication.java
+++ src/main/java/study/oopstudy/OopStudyApplication.java
@@ -1,13 +1,23 @@
 package study.oopstudy;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.util.Scanner;
+
 public class OopStudyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OopStudyApplication.class, args);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("게임을 시작하시겠습니까?");
+		System.out.println("예 : 1 / 아니오 : 2");
+		System.out.printf("입력 : ");
+		int choice = sc.nextInt();
+
+		if (choice < 1 || choice > 2) {
+
+			throw new IllegalArgumentException("잘못 입력하였습니다. 게임을 종료합니다.");
+		}
+
 	}
 
 }
