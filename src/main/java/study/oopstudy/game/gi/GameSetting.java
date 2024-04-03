2f82852659a7a329bc2c5b9571dc79548c163030
MoonByungHoon <mbh1069@naver.com>
2024-04-03 11:41:36 +0900

    feat: 자동차 경주 기능 구현
    Game - 게임 진행의 전반적 흐름 제어. GameBroadCast, GameSetting을
    상속받음.
    
    GameBroadCast, GameSetting - 게임 진행에 관련된 인터페이스.
    
    Player - 게임 진행에 필요한 플레이어 정보. Move를 상속받음.
    
    Move - 게임 진행에 필요한 승점 포인트를 관리함.
    
    객체 관계도 - ![image](https://github.com/MoonByungHoon/OOP-Study/assets/106061341/0d0cdb9e-880c-4b71-862b-c05c218e932e)

--- src/main/java/study/oopstudy/game/gi/GameSetting.java
+++ src/main/java/study/oopstudy/game/gi/GameSetting.java
@@ -1,2 +1,9 @@
-package study.oopstudy.Game;public interface GameSetting {
+package study.oopstudy.game.gi;
+
+import study.oopstudy.player.Player;
+
+import java.util.List;
+
+public interface GameSetting {
+  List<Player> setPlayers(String[] nameSplit);
 }
